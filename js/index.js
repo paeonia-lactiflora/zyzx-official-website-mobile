41eddd4f20ff4132490b59b96a1c987079e77e74
实验室207 <1619649679@qq.com>
2024-05-11 00:05:58 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   css/common.css
    	new file:   css/community.css
    	new file:   css/healthy.css
    	new file:   css/mall.css
    	new file:   css/mine.css
    	modified:   html/community.html
    	modified:   html/healthy.html
    	modified:   html/mall.html
    	new file:   html/mine.html
    	new file:   img/community/bg.png
    	new file:   img/healthy/bg.png
    	new file:   img/index/apparel/bg.png
    	renamed:    img/appointment/xxyy-bg.png -> img/index/appointment/xxyy-bg.png
    	renamed:    img/appointment/xxyy-icon.png -> img/index/appointment/xxyy-icon.png
    	renamed:    img/appointment/yygh-bg.png -> img/index/appointment/yygh-bg.png
    	renamed:    img/appointment/yygh-btn.png -> img/index/appointment/yygh-btn.png
    	renamed:    img/appointment/yygh-icon.png -> img/index/appointment/yygh-icon.png
    	renamed:    img/appointment/zjjj-bg.png -> img/index/appointment/zjjj-bg.png
    	renamed:    img/appointment/zjjj-icon.png -> img/index/appointment/zjjj-icon.png
    	renamed:    img/banner/banner1.png -> img/index/banner/banner1.png
    	renamed:    img/banner/banner2.png -> img/index/banner/banner2.png
    	renamed:    img/banner/banner3.png -> img/index/banner/banner3.png
    	renamed:    img/question/star.png -> img/index/question/star.png
    	renamed:    img/question/upvote.png -> img/index/question/upvote.png
    	renamed:    img/recommend-doctor/header-bg.png -> img/index/recommend-doctor/header-bg.png
    	renamed:    img/recommend-doctor/jyy.png -> img/index/recommend-doctor/jyy.png
    	renamed:    img/recommend-doctor/stars.png -> img/index/recommend-doctor/stars.png
    	renamed:    img/tool/jbdt.png -> img/index/tool/jbdt.png
    	renamed:    img/tool/jkzc.png -> img/index/tool/jkzc.png
    	renamed:    img/tool/jkzx.png -> img/index/tool/jkzx.png
    	renamed:    img/tool/smzs.png -> img/index/tool/smzs.png
    	renamed:    img/tool/xljk.png -> img/index/tool/xljk.png
    	renamed:    img/tool/yfjc.png -> img/index/tool/yfjc.png
    	renamed:    img/tool/znpf.png -> img/index/tool/znpf.png
    	renamed:    img/tool/znsb.png -> img/index/tool/znsb.png
    	renamed:    img/tool/znwz.png -> img/index/tool/znwz.png
    	new file:   img/mall/bg.png
    	new file:   img/mine/bg.png
    	modified:   index.html
    	modified:   js/index.js
    	modified:   less/common.less
    	new file:   less/community.less
    	new file:   less/healthy.less
    	new file:   less/mall.less
    	new file:   less/mine.less

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const qst_clsfct_item_list = document.querySelectorAll(".question .classification .item")
+
+for (let i = 0; i < qst_clsfct_item_list.length; i++) {
+	qst_clsfct_item_list[i].addEventListener('click', () => {
+		if (document.querySelector('.question .classification .item.active')) {
+			document.querySelector('.question .classification .item.active').classList.remove('active')
+		}
+
+		qst_clsfct_item_list[i].classList.toggle("active")
+	})
+}
