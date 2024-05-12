46de571bfa34dfa97eae14f573d9f5da7b7923cd
实验室207 <1619649679@qq.com>
2024-05-12 16:50:45 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   css/common.css
    	new file:   css/todo.css
    	modified:   html/community.html
    	modified:   html/healthy.html
    	modified:   html/mall.html
    	modified:   html/mine.html
    	deleted:    html/other.html
    	new file:   html/todo.html
    	new file:   img/todo_page/todo_bg.png
    	modified:   index.html
    	modified:   js/common.js
    	modified:   less/common.less
    	new file:   less/todo.less

--- js/common.js
+++ js/common.js
@@ -1,9 +1,23 @@
-// 当页面加载完成
-window.addEventListener('load', function () {
-	// 隐藏 loading 动画，因为背景图已经加载完成
-	console.log('页面加载完成')
-	setTimeout(() => {
-		console.log('延迟隐藏 loading 动画')
-		document.querySelector('.loader-container').style.display = 'none'
-	}, 1000)
-})
+if (document.querySelector('.loader-container')) { // 5个 tabBar 页面
+	// 当页面加载完成
+	window.addEventListener('load', function () {
+		// 隐藏 loading 动画，因为背景图已经加载完成
+		console.log('页面加载完成')
+		setTimeout(() => {
+			console.log('延迟隐藏 loading 动画')
+			document.querySelector('.loader-container').style.display = 'none'
+		}, 1000)
+	})
+
+	// 除index.html以外的4个图片页面的body
+	if (document.querySelector('body.empty')) {
+		document.querySelector('body.empty').addEventListener('click', function (e) {
+			if (e.target.classList.contains('loader-container') || e.target.classList.contains('tabBar')) return
+
+			console.log('body 被点击')
+			
+			// 跳转至todo.html
+			window.location.href = '/html/todo.html'
+		})
+	}
+}
